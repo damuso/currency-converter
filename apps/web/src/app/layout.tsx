86d8c3c9bdc66a0a3b9041f9f2b4894fef93ec7d
Damián Paranič <damuso15@gmail.com>
2025-07-25 20:47:23 +0200

    refactor: update file naming to match project convention

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-import LayoutFooter from '@/components/layout/LayoutFooter'
-import LayoutHeader from '@/components/layout/LayoutHeader'
-import { ThemeProvider } from '@/components/theme/ThemeProvider'
+import LayoutFooter from '@/components/layout/layout-footer'
+import LayoutHeader from '@/components/layout/layout-header'
+import { ThemeProvider } from '@/components/theme/theme-provider'
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
--- apps/web/src/components/layout/layout-header.tsx
+++ apps/web/src/components/layout/layout-header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import ThemeSwitcher from '@/components/theme/ThemeSwitcher'
+import ThemeSwitcher from '@/components/theme/theme-switcher'
 
 const LayoutHeader = () => {
 	return (
